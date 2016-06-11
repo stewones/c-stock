29e2ca245284d3edf4c64b86ef537068acf7f43a
Stewan Pacheco <talk@stpa.co>
2016-06-11 13:22:59 -0300

    feat(report): relatorio 1 dos produtos

--- main.c
+++ main.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <conio.h>
 //define as variaveis de escopo global da app
-
 #define PRODUCT_LIMIT 100
 int MENU = 0; //guarda menu do momento
 int PRODUCT_CODES[PRODUCT_LIMIT]; //vetor de codigos
@@ -11,28 +9,34 @@ float PRODUCT_PRICES[PRODUCT_LIMIT]; //vetor de preços
 
 int main()
 {
-    system("cls");
-    do {
+    do
+    {
         //exibe opcoes do menu e atribui o numero da opc na variavel MENU
         menu_choose();
         //executa a ação do menu
-        switch(MENU) {
-            case 1:
-                system("cls");
-                add_product();
+        switch (MENU)
+        {
+        case 1:
+            system("cls");
+            add_product();
             break;
-            case 2:
-                system("cls");
-                edit_product();
+        case 2:
+            system("cls");
+            edit_product();
             break;
-            case 3:
-                system("cls");
-                delete_product();
+        case 3:
+            system("cls");
+            delete_product();
             break;
-            default:
-                system("exit");
+        case 4:
+            system("cls");
+            report_product(1);
+            break;
+        default:
+            system("exit");
         }
-    } while (MENU>0 && MENU <=5);
+    }
+    while (MENU>0 && MENU <=5);
 
     printf("---------------\n");
     printf(" Opc invalida! \n");
@@ -40,10 +44,11 @@ int main()
 }
 
 //opcs do menu
-int menu_choose() {
+int menu_choose()
+{
+    system("cls");
     printf("\n");
     printf("\n");
-    printf(" Selecione uma opc no menu: \n\n");
     printf(" Produtos \n");
     printf("--------------------------\n");
     printf(" 1 - Incluir cadastro \n");
@@ -58,26 +63,34 @@ int menu_choose() {
     printf("--------------------------\n");
     printf(" 0 - Voltar ao menu principal \n");
     printf("\n");
+    printf("\n");
+    printf(" Digite uma opc valida: ");
     scanf("%i",&MENU);
 }
 
 //adiciona novo produto
-int add_product() {
+int add_product()
+{
     int code;
     printf("\n");
     printf(" Insira o codigo do produto: ");
     scanf("%i",&code);
 
     //se nao tiver codigo digitado, volta pro menu principal
-    if (!code) {
-       main();
-    }else{
+    if (!code)
+    {
+        main();
+    }
+    else
+    {
         //verifica se ja nao existe codigo de produto cadastrado
-        if (!product_exists(code)) {
+        if (!product_exists(code))
+        {
             //pegar preço
             float price = get_price();
             int stock = get_stock();
-            if (price && stock) {
+            if (price && stock)
+            {
                 //pega a proxima posição disponivel no vetor
                 int New = next_position();
 
@@ -97,20 +110,27 @@ int add_product() {
                 int continues = 1;
                 scanf("%i",&continues);
 
-                if(continues) {
+                if (continues)
+                {
                     system("cls");
                     add_product();
-                }else{
+                }
+                else
+                {
                     main();
                 }
-            }else{
+            }
+            else
+            {
                 printf("\n");
                 printf("------------------------\n");
                 printf(" Erro: Valor incorreto! \n");
                 printf("------------------------\n");
                 add_product(); //reinicia logica de inserção do produto
             }
-        }else{
+        }
+        else
+        {
             system("cls");
             printf("-----------------------------------\n");
             printf(" Este codigo de produto ja existe! \n");
@@ -121,26 +141,33 @@ int add_product() {
 }
 
 //edita um produto
-int edit_product() {
+int edit_product()
+{
     int code;
     printf("\n");
     printf(" Insira o codigo do produto: ");
     scanf("%i",&code);
 
     //se nao tiver codigo digitado, volta pro menu principal
-    if (!code) {
+    if (!code)
+    {
         main();
-    }else{
+    }
+    else
+    {
         //pego a posicao do codigo do produto, caso ele existir
         int exists = get_position(code);
         //se nao existir produto
-        if (exists == -1) {
+        if (exists == -1)
+        {
             system("cls");
             printf("------------------------------------\n");
             printf(" Este codigo de produto nao existe! \n");
             printf("------------------------------------\n");
             edit_product(); //recomeço a edição
-        }else{
+        }
+        else
+        {
             system("cls");
             printf("----------------------------------------------\n");
             printf(" Produto encontrado, prossiga com a alteracao \n");
@@ -149,7 +176,8 @@ int edit_product() {
             float price = get_price();
             int stock = get_stock();
 
-            if (price && stock) {
+            if (price && stock)
+            {
                 //salva nos vetores
                 PRODUCT_PRICES[exists] = price;
                 PRODUCT_STOCK[exists] = stock;
@@ -165,13 +193,18 @@ int edit_product() {
                 int continues = 1;
                 scanf("%i",&continues);
 
-                if(continues) {
+                if (continues)
+                {
                     system("cls");
                     edit_product();
-                }else{
+                }
+                else
+                {
                     main();
                 }
-            }else{
+            }
+            else
+            {
                 system("cls");
                 printf("--------------------------\n");
                 printf(" Erro: valores incorretos \n");
@@ -183,102 +216,146 @@ int edit_product() {
 }
 
 //deleta um produto
-int delete_product() {
+int delete_product()
+{
     int code;
     printf("\n");
     printf(" Insira o codigo do produto a ser excluido: ");
     scanf("%i",&code);
 
     //se nao tiver codigo digitado, volta pro menu principal
-    if (!code) {
+    if (!code)
+    {
         main();
-    }else{
+    }
+    else
+    {
         //pego a posicao do codigo do produto, caso ele existir
         int exists = get_position(code);
         //se nao existir produto
-        if (exists == -1) {
+        if (exists == -1)
+        {
             system("cls");
             printf("------------------------------------\n");
             printf(" Este codigo de produto nao existe! \n");
             printf("------------------------------------\n");
             edit_product(); //recomeço a edição
-        }else{
+        }
+        else
+        {
             system("cls");
+            printf("\n");
             printf("-----------------------------------------------------------------------\n");
             printf(" Produto encontrado, tem certeza que deseja prosseguir com a exclusao? \n");
             printf("-----------------------------------------------------------------------\n");
-            printf(" Deseja alterar outro produto? \n\n 1 - sim | 0 - nao => ");
+            printf(" 1 - sim | 0 - nao => ");
 
-             //pergunta se quer continuar
-             int continues = 0;
-             scanf("%i",&continues);
-             if(continues) {
-                 while (exists < product_total()) {
+            //pergunta se quer continuar
+            int continues = 0;
+            scanf("%i",&continues);
+            if (continues)
+            {
+                //logica da exclusao consiste em reposicionar o vetor
+                //colocando o item da direita pra esquerda a partir da posicao que deseja excluir
+                while (exists < product_total())
+                {
                     PRODUCT_PRICES[exists] = PRODUCT_PRICES[exists+1];
                     PRODUCT_STOCK[exists] = PRODUCT_STOCK[exists+1];
                     PRODUCT_CODES[exists] = PRODUCT_CODES[exists+1];
                     exists++;
                 }
-
                 system("cls");
                 printf("--------------------------------\n");
                 printf(" Produto excluido com sucesso ! \n");
                 printf("--------------------------------\n");
                 main();
-
-            }else{
+            }
+            else
+            {
                 main(); //retorna ao menu principal
             }
         }
     }
 }
 
+//relatorio de produtos
+int report_product(type)
+{
+    switch (type)
+    {
+    case 1: //relatorio 1
+        order_asc();
+        int i;
+        int total = product_total();
+        for (i = 0; i < total; i++)
+        {
+            printf("\n---------------------------------\n");
+            printf(" Produto codigo: %i \n Valor: R$ %02f \n Estoque: %i", PRODUCT_CODES[i], PRODUCT_PRICES[i], PRODUCT_STOCK[i]);
+            if (i == total-1) {
+                printf("\n---------------------------------\n");
+                printf("\n");
+            }
+        }
+        break;
+    }
+    system("pause");
+}
+
 
 //retorna total de produtos cadastrados
-int product_total() {
+int product_total()
+{
     int i, total = 0;
-    for (i=0;i<PRODUCT_LIMIT;i++) {
-      if (PRODUCT_CODES[i]) total++;
+    for (i=0;i<PRODUCT_LIMIT;i++)
+    {
+        if (PRODUCT_CODES[i]) total++;
     }
     return total;
 }
 
 
 //verifica se existe codigo de produto cadastrado
-int product_exists(code) {
+int product_exists(code)
+{
     int i;
     //loop pra verificar se existe produto
-    for (i=0;i<PRODUCT_LIMIT;i++) {
+    for (i=0;i<PRODUCT_LIMIT;i++)
+    {
 
-      //caso existir, retorna a posicao da existencia.
-      if (PRODUCT_CODES[i] == code) return 1;
+        //caso existir, retorna a posicao da existencia.
+        if (PRODUCT_CODES[i] == code) return 1;
     }
     //caso nao existir, retorna 0. equivalente a true
     return 0;
 }
 
-//retorna a proxima posicao disponivel no vetor de codigos
-int next_position() {
+//retorna a proxima posicao disponivel para cadastrar o novo produto
+int next_position()
+{
     int i;
     //loop pra pegar proxima posicao disponivel
-    for (i=0;i<PRODUCT_LIMIT;i++) {
-      if (!PRODUCT_CODES[i]) return i;
+    for (i=0;i<PRODUCT_LIMIT;i++)
+    {
+        if (!PRODUCT_CODES[i]) return i;
     }
     return 0;
 }
 
 //retorna a posicao do codigo solicitado
-int get_position(code) {
+int get_position(code)
+{
     int i;
     //loop pra pegar proxima posicao disponivel
-    for (i=0; i<PRODUCT_LIMIT; i++) {
-      if (PRODUCT_CODES[i] == code) return i;
+    for (i=0; i<PRODUCT_LIMIT; i++)
+    {
+        if (PRODUCT_CODES[i] == code) return i;
     }
     return -1;
 }
 
 //pegar preço do produto e verificar se eh valido
-int get_price() {
+int get_price()
+{
     float price;
     printf("\n");
     printf(" Valor do produto em R$: ");
@@ -291,7 +368,8 @@ int get_price() {
 }
 
 //pegar quantidade do stock e verificar se eh valido
-int get_stock() {
+int get_stock()
+{
     int stock;
     printf("\n");
     printf(" Quantidade no estoque: ");
@@ -301,4 +379,61 @@ int get_stock() {
         return stock;
     else
         return 0;
+}
+
+//ordenar crescente
+int order_asc()
+{
+    int total = product_total();
+    int x, y;
+
+    //loop enquanto x for menor que o total de itens cadastrados
+    for (x=0; x<total; x++)
+    {
+
+        //loop enquanto y menor que o total -1
+        for (y=0; y<total-1; y++)
+        {
+            //se o codigo atual for maior que o proximo codigo
+            if (PRODUCT_CODES[y]>PRODUCT_CODES[y+1])
+            {
+                //reordenar os codigos
+
+                //guardo um temporario com o codigo do proximo
+                int temp = PRODUCT_CODES[y+1];
+
+                //falo que o proximo vai valer o atual
+                PRODUCT_CODES[y+1] = PRODUCT_CODES[y];
+
+                //coloco valor guardado no atual
+                PRODUCT_CODES[y] = temp;
+
+                //reordenar o estoque
+
+                //guardo um temporario com o codigo do proximo
+                int temp_stock = PRODUCT_STOCK[y+1];
+
+                //falo que o proximo vai valer o atual
+                PRODUCT_STOCK[y+1] = PRODUCT_STOCK[y];
+
+                //coloco valor guardado no atual
+                PRODUCT_STOCK[y] = temp_stock;
+
+                //reordenar os preços
+
+                //guardo um temporario com o codigo do proximo
+                int temp_price = PRODUCT_PRICES[y+1];
+
+                //falo que o proximo vai valer o atual
+                PRODUCT_PRICES[y+1] = PRODUCT_PRICES[y];
+
+                //coloco valor guardado no atual
+                PRODUCT_PRICES[y] = temp_price;
+
+            }
+
+        }
+
+    }
+
 }
